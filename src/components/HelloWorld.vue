8af0b2cb84b15ed86f7a2421cc4438b7b3e5c2c5
Zeqing-Wang <920626166@qq.com>
2023-02-06 02:21:45 +0800

    frame ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,145 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <img src="../assets/jlu.jpg" >
+    <h1>基于语音特征与身份ID的3D人脸动画生成系统</h1>
+    王泽卿， 吉林大学 软件学院
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      一段关于该任务的介绍视频，非常详细的阐述了“语音驱动”这一工作的目的，以及应用场景。
+      <a href="https://github.com/pettarin/forced-alignment-tools" target="_blank" rel="noopener">视频来源于Speech Driven Tongue Animation(CVPR,2022)</a>.
     </p>
-    <el-switch v-model="value1" />
-    <el-switch
-    v-model="value2"
-    class="ml-2"
-    style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
-    />
-    <el-button type="primary">生成3D人脸动画d </el-button>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <video controls width="500">
+        <source src="/media/cc0-videos/flower.webm" type="video/webm">
+
+        <source src="../assets/tongue-anim.mp4" type="video/mp4">
+
+        Download the
+        <a href="/media/cc0-videos/flower.webm">WEBM</a>
+        or
+        <a href="/media/cc0-videos/flower.mp4">MP4</a>
+        video.
+    </video>
+
+    <div>
+      <el-divider />
+    </div>
+
+    <h2>只需3步，尝试制作属于你的3D人脸动画！</h2>
+
+    <el-steps :active="1" align-center>
+      <el-step title="第一步" description="上传一段音频，并根据需要，选择是否通过文本协助内容生成。" />
+      <el-step title="第二步" description="选择生成的风格以及生成ID，即3D人脸动画的生成风格以及生成的人物ID。" />
+      <el-step title="第三步" description="提交数据，等待您的3D人脸动画生成！" />
+    </el-steps>
+
+    <h3>第一步，上传您的音频,并根据选择输入文字</h3>
+    <p></p>
+    <el-upload
+      v-model:file-list="fileList"
+      class="upload-wav"
+      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+      multiple
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :before-remove="beforeRemove"
+      :limit="3"
+      :on-exceed="handleExceed"
+    >
+    <el-button type="primary">上传音频。</el-button>
+      <template #tip>
+        <div class="el-upload__tip">
+          wav音频文件大小应不超过5MB
+        </div>
+        <el-input v-model="input" placeholder="请输入与音频相对应的文字" />
+      </template>
+    </el-upload>
+
+    <p></p>
+
+    <el-steps :active="2" align-center>
+      <el-step title="第一步" description="上传一段音频，并根据需要，选择是否通过文本协助内容生成。" />
+      <el-step title="第二步" description="选择生成的风格以及生成ID，即3D人脸动画的生成风格以及生成的人物ID。" />
+      <el-step title="第三步" description="提交数据，等待您的3D人脸动画生成！" />
+    </el-steps>
+
+    <h3>第二步，选择您想要生成的风格以及ID</h3>
+    <p></p>
+    <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+
+    <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+
+    <p></p>
+
+    <el-steps :active="3" align-center>
+      <el-step title="第一步" description="上传一段音频，并根据需要，选择是否通过文本协助内容生成。" />
+      <el-step title="第二步" description="选择生成的风格以及生成ID，即3D人脸动画的生成风格以及生成的人物ID。" />
+      <el-step title="第三步" description="提交数据，等待您的3D人脸动画生成！" />
+    </el-steps>
+
+    <h3>第三步，提交数据，并等待您的3D人脸动画生成！</h3>
+    <p></p>
+    <div>
+      <el-button type="primary" disabled>生成</el-button>
+    </div>
+    <p></p>
+    <video controls width="500">
+        <source src="/media/cc0-videos/flower.webm" type="video/webm">
+
+        <source src="../assets/test_FaceTalk_170809_00138_TA_condition_FaceTalk_170913_03279_TA.mp4" type="video/mp4">
+
+        Download the
+        <a href="/media/cc0-videos/flower.webm">WEBM</a>
+        or
+        <a href="/media/cc0-videos/flower.mp4">MP4</a>
+        video.
+    </video>
+
+
   </div>
 </template>
 <script setup>
+// import { Edit, Picture, UploadFilled } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 
-const value1 = ref(true)
-const value2 = ref(true)
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+
 </script>
 
 <!-- <script>
